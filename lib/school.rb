24d864101265a711326e85e94ef21444865df37e
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-27 19:52:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,10 @@
 class School
 
-attr_reader :name
+attr_reader :name, :roster
 
 def initialize(name)
   @name = name
-end
-
-def roster 
-  roster = {}
+  @roster = {}
 end
 
 def add_student(student, grade)
